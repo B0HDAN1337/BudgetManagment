38159a467a4e26e906b1ad0f7918eedf11e2a7b8
ghost <machuliak1337@gmail.com>
2025-04-08 23:58:58 +0200

    Implement API requests for User table

---
+++ Data/DBInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManagmentServer.Data
+{
+    public static class DBInitializer
+    {
+        public static void Initializer(BudgetManagmentContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
