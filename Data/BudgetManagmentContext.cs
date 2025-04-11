2962222d4f78afe92f9bbd9e93241b5030b4bea0
B0HDAN1337 <148467696+B0HDAN1337@users.noreply.github.com>
2025-04-11 21:29:56 +0200

    Implement API requests for User table (#19)

---
+++ Data/BudgetManagmentContext.cs
@@ -0,0 +1,22 @@
+﻿using BudgetManagmentServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetManagmentServer.Data
+{
+    public class BudgetManagmentContext : DbContext
+    {
+        public BudgetManagmentContext(DbContextOptions<BudgetManagmentContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+    }
+}
