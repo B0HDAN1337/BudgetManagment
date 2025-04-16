caba9b66f4a2e68fc4341978122ff814adffccc7
ghost <machuliak1337@gmail.com>
2025-04-16 12:50:03 +0200

    Added simple creating wallet  page

--- BudgetManagment-app/src/app/app.routes.ts
+++ BudgetManagment-app/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { UserLogInComponent } from './user-log-in/user-log-in.component';
 import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
 import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { WalletCreationComponent } from './wallet-creation/wallet-creation.component';
 
 export const routes: Routes = [
     {path: 'user-log-in', component: UserLogInComponent},
     {path: 'user-sign-up', component: UserSignUpComponent},
-    {path: 'overview-page', component: OverviewPageComponent}
+    {path: 'overview-page', component: OverviewPageComponent},
+    {path: 'wallet-creation', component: WalletCreationComponent}
 ];
