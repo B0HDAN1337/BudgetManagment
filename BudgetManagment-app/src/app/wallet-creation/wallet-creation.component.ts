578dcf0fed595b75c33522b0f41fe53995c0b6e7
B0HDAN1337 <148467696+B0HDAN1337@users.noreply.github.com>
2025-04-16 12:56:02 +0200

    Add post query, overview and creating wallet pages (#39)
    * Changed model User
    
    * Create user in Sign Up page (#31)
    
    * Added Cors to allow website on localhost:4200 send requests
    
    * Added function to create user in sign-up page using post request
    
    * import new necessary libraries
    
    * Add post query, overview and creating wallet pages (#38)
    
    * Added Cors to allow website on localhost:4200 send requests
    
    * Added function to create user in sign-up page using post request
    
    * import new necessary libraries
    
    * Added simple overview page
    
    * Changed to relative page
    
    * changed frontend width for smaller devices
    
    * Added simple creating wallet  page

---
+++ BudgetManagment-app/src/app/wallet-creation/wallet-creation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-creation',
+  imports: [],
+  templateUrl: './wallet-creation.component.html',
+  styleUrl: './wallet-creation.component.css'
+})
+export class WalletCreationComponent {
+
+}
