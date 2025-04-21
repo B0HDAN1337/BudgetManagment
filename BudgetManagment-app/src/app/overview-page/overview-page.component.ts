0b05692f6634c02fef27e44941554d559bf57667
ghost <machuliak1337@gmail.com>
2025-04-22 01:09:55 +0200

    Added transaction menu in overview page

--- BudgetManagment-app/src/app/overview-page/overview-page.component.ts
+++ BudgetManagment-app/src/app/overview-page/overview-page.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-overview-page',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './overview-page.component.html',
   styleUrl: './overview-page.component.css'
 })
 export class OverviewPageComponent {
 
+  isMenuVisible = false;
+
+  OpenMenu()
+  {
+    this.isMenuVisible = true;
+  }
+
+  CloseMenu()
+  {
+    this.isMenuVisible = false;
+  }
 }
