6b654981f7cc72df178e7d7e60dc77cf55112c06
ghost <machuliak1337@gmail.com>
2025-04-23 10:00:53 +0200

    Add post request to create new wallet

--- BudgetManagment-app/src/app/wallet-creation/wallet-creation.component.ts
+++ BudgetManagment-app/src/app/wallet-creation/wallet-creation.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-wallet-creation',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './wallet-creation.component.html',
   styleUrl: './wallet-creation.component.css'
 })
 export class WalletCreationComponent {
 
+  WalletObj: any =
+  {
+  "WalletName": "",
+  "Description": "",
+  "Currency": ""
+  }
+
+  urlCreateWallet = 'http://localhost:5142/api/Wallet';
+
+  constructor(private http: HttpClient) {}
+
+  OnCreateWallet()
+  {
+    this.CreatingWallet();
+  }
+
+
+  CreatingWallet()
+  {
+    this.http.post(this.urlCreateWallet, this.WalletObj).subscribe( success =>
+      {
+        console.log("Success wallet", success);
+        alert("Success created wallet");
+      }, error =>
+      {
+        console.log("Error wallet", error);
+        alert("Error to create wallet");
+      });
+  }
 }
