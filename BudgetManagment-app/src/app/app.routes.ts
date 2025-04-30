fe3c75bef419d580143f2b440bd5b271ca3ecbb3
ghost <machuliak1337@gmail.com>
2025-04-30 14:36:17 +0200

    Add security (user can go to the another page, if have token)

--- BudgetManagment-app/src/app/app.routes.ts
+++ BudgetManagment-app/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { UserLogInComponent } from './user-log-in/user-log-in.component';
 import { UserSignUpComponent } from './user-sign-up/user-sign-up.component';
 import { OverviewPageComponent } from './overview-page/overview-page.component';
 import { WalletCreationComponent } from './wallet-creation/wallet-creation.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: 'user-log-in', component: UserLogInComponent},
     {path: 'user-sign-up', component: UserSignUpComponent},
-    {path: 'overview-page', component: OverviewPageComponent},
-    {path: 'wallet-creation', component: WalletCreationComponent}
+    {path: 'overview-page', component: OverviewPageComponent, canActivate: [authGuard]},
+    {path: 'wallet-creation', component: WalletCreationComponent, canActivate: [authGuard]}
 ];
